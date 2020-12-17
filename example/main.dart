7f86cd47daeb64bf321e35127fa744534b45640b
Vinay Jain <vinayjain7281@gmail.com>
2020-12-17 12:34:31 +0530

    example added

---
+++ example/main.dart
@@ -0,0 +1,68 @@
+import 'package:custom_splash_screens/custom_splash_screens.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // SCALE_SPLASH_SCREEN
+      /*
+      home: ScaleSplashScreen(
+      icon: FlutterLogo(),
+      iconScaleDuration: Duration(seconds: 2),
+      label: "APP NAME",
+      labelDirection: SplashScreenDirection.LTR,
+      labelDuration: Duration(seconds: 2),
+      navigateTo: NavigateTo(),
+      screenFunction: () async {
+        await Future.delayed(Duration(seconds: 5));
+      },
+      screenLoader: SizedBox(
+        height: 100,
+        width: 100,
+        child: Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    )
+      */
+      // LINEAR_SPLASH_SCREEN
+
+      home: LinearSplashScreen(
+        icon: FlutterLogo(),
+        iconDirection: SplashScreenDirection.TTB,
+        iconDuration: Duration(seconds: 2),
+        label: "APP NAME",
+        labelDirection: SplashScreenDirection.LTR,
+        labelDuration: Duration(seconds: 2),
+        navigateTo: NavigateTo(),
+        screenFunction: () async {
+          await Future.delayed(Duration(seconds: 5));
+        },
+        screenLoader: SizedBox(
+          height: 100,
+          width: 100,
+          child: Center(
+            child: CircularProgressIndicator(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class NavigateTo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("NEW PAGE")),
+      body: Container(
+        child: Center(
+          child: Text("NEW PAGE"),
+        ),
+      ),
+    );
+  }
+}
